38d3cf0a91ee34ec2c0d6f2cb3d51f33fe968ee1
Vertex101 <vertex101@gmx.com>
2019-12-31 23:42:02 -0500

    test
    testing code

--- app.js
+++ app.js
@@ -1,37 +1,28 @@
-const Discord = require("discord.js");
+const { Client, RichEmbed } = require("discord.js");
 const request = require('request');
-const client = new Discord.Client();
-
+const client = new Client();
 var prefix = "!";
-
 client.on("ready", () => {
     // This event will run if the bot starts, and logs in, successfully.
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
     client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
-
 client.on("guildCreate", guild => {
     // This event triggers when the bot joins a guild.
     console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
     client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
-
 client.on("guildDelete", guild => {
     // this event triggers when the bot is removed from a guild.
     console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
     client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
-
 client.on("message", async message => {
     if(message.author.bot) return;
-    
     if(message.content.indexOf(prefix) !== 0) return;
-    
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-    
     // Let's go with a few common example commands! Feel free to delete or change those.
-    
     if(command === "ex") {
         request('https://api.poe.watch/item?id=142', function (error, response, body) {
             pullData = JSON.parse(body);
@@ -60,6 +51,25 @@ client.on("message", async message => {
         } else {
             message.channel.send("Usage: !round [1-9]"); 
         }
+    }
+    if(command == "20") {
+        const embed = new RichEmbed()
+            // Set the title of the field
+            .setTitle('Top 5 21/20 Gems')
+            // Set the color of the embed
+            .setColor(0xFF0000)
+            // Set the main content of the embed
+            .setDescription('Hello, this is a slick embed!');
+        // Send the embed to the same channel as the message
+        message.channel.send(embed);
+        /* request("https://api.poe.watch/get?league=Metamorph&category=gem", function (error, responce, body) {
+            top520 = JSON.parse(body);
+            top520.forEach(function (fruit) {
+                if(fruit.gemLevel == "21" && fruit.gemQuality == "20") { //&& fruit.change != "0"
+                    gem2120.push(fruit.name+":"+fruit.exalted.toFixed(2))
+                }
+            });
+        }); */
     }
 });
   
